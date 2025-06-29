f54ec38447288bbfef4885a1d6a1c70d8d3ab8c1
Carlos Fuentealba <carlos@fuentealba.dev>
2025-06-29 18:53:46 -0400

    Implementa autenticación con NextAuth, configurando proveedores y adaptador de Prisma. Se define el manejo de sesiones para incluir el ID del usuario en la sesión.

---
+++ src/lib/auth.ts
@@ -0,0 +1,26 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./prisma";
+import GoogleProvider from "next-auth/providers/google";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.sub as string;
+      }
+      return session;
+    },
+  },
+}; 
